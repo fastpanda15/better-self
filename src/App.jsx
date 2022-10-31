9d59106f7274f163ad8b0cdeceb552ef7bae1c09
Tyler Bordeaux <51490846+fastpanda15@users.noreply.github.com>
2022-10-31 18:15:19 -0400

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useRef } from 'react';
+import content from './content2.png';
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+function App() {
+  const ref = useRef();
+
+  return (
+    <div>
+      <Parallax pages={4} ref={ref}>
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundColor: '#F8CA9A',
+            backgroundImage: `url(${content})`,
+            backgroundSize: 'contain',
+          }}
+        >
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <h2>Get better everyday</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundColor: '#F8CA9A',
+            backgroundSize: 'cover',
+          }}
+        >
+          
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{ start: 1, end: 4.5 }}
+          style={{ textAlign: 'center' }}
+        >
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={0.1}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h3>by Tyler Bordeaux</h3>
+          <h4>tylerbordeaux06@icloud.com</h4>
+        
+        </ParallaxLayer>
+      </Parallax>
+    </div>
+  );
+}
+
+export default App;
